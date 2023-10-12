261972c0a514128d3b5d725a0337789365dea064
Aryan95614 <64114240+Aryan95614@users.noreply.github.com>
2023-10-11 22:17:32 -0400

    Udemy Learning Material

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 1, 1, 1};
-        System.out.println(arr[0]);
+
+        System.out.println(0);
     }
 }
 
