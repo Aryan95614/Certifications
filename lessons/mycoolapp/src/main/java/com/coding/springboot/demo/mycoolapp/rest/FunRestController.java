9d19df6b59eaaacf1f981fa4906b1bed376b5dc6
Aryan95614 <64114240+Aryan95614@users.noreply.github.com>
2023-10-12 12:38:31 -0400

    Hopefully this works

--- lessons/mycoolapp/src/main/java/com/coding/springboot/demo/mycoolapp/rest/FunRestController.java
+++ lessons/mycoolapp/src/main/java/com/coding/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,11 +1,16 @@
 package com.coding.springboot.demo.mycoolapp.rest;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+
+    @Value("${coach.name}")
+    private String coachName;
 
     @GetMapping("/")
     public String sayHello() {
