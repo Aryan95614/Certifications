8865de118e230d8e1bac7c4a4eb85917a6960a31
Aryan95614 <64114240+Aryan95614@users.noreply.github.com>
2023-10-13 11:47:48 -0400

    Learning more about Java and stuff

---
+++ lessons/2. Actuator and Beyond/src/main/java/com/example/learningAcutatorAndBeyond/LearningAcutatorAndBeyondApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningAcutatorAndBeyond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningAcutatorAndBeyondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningAcutatorAndBeyondApplication.class, args);
+	}
+
+}
---
+++ lessons/2. Actuator and Beyond/src/test/java/com/example/learningAcutatorAndBeyond/LearningAcutatorAndBeyondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningAcutatorAndBeyond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningAcutatorAndBeyondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
