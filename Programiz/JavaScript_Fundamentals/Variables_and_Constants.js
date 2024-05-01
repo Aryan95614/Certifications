cefda546aa2a4bae962cf038085cabb13f0909a8
Aryan Dhawan <aryansdhawan@gmail.com>
2024-04-30 22:52:44 -0400

    Finished Section 2
    Learned the Basics of HTML5

---
+++ Programiz/JavaScript_Fundamentals/Variables_and_Constants.js
@@ -0,0 +1,24 @@
+
+// here num is a variable that stores the number 5
+let num = 5;
+
+// Declaring Variables
+var age;
+let name;
+
+/*
+ The var is used in older versions of Javascript. 
+
+
+ Variables created with var are function-scoped, meaning they can be accessed wherever in the function, but no where else.
+
+ --
+
+ let is the new way of declaring variables, starting with ES6 (ES2015)
+
+ Variables declared with let are block-scoped, meaning they can be accessed within the block in which they are declared
+
+
+ NTB: It is recommended to use let as it has more browser support.
+*/
+
